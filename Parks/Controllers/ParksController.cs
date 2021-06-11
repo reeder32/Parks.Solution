134e5e564f5bab0f0f8b249a89cd1726ba57feb0
Nicholas Reeder <nickreeder32@gmail.com>
2021-06-11 08:51:53 -0700

    :card_file_box: add delete route to controller

--- Parks/Controllers/ParksController.cs
+++ Parks/Controllers/ParksController.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 using Parks.Models;
 
 namespace Parks.Controllers
@@ -15,27 +16,17 @@ namespace Parks.Controllers
     {
       _db = db;
     }
+     private bool ParkExists(int id)
+    {
+      return _db.Parks.Any(e => e.ParkId == id);
+    }
     // GET api/parks
     [HttpGet]
     public async Task<ActionResult<IEnumerable<Park>>> Get()
     {
       return await _db.Parks.ToListAsync();
     }
-    // Get individual park with id ap/parks/id
-    [HttpGet("{id}")]
-public async Task<ActionResult<Park>> GetPark(int id)
-{
-    var park = await _db.Parks.FindAsync(id);
-
-    if (park == null)
-    {
-        return NotFound();
-    }
-
-    return park;
-}
-
-    // POST api/parks
+     // POST api/parks
     [HttpPost]
     public async Task<ActionResult<Park>> Post(Park park)
     {
@@ -44,5 +35,66 @@ public async Task<ActionResult<Park>> GetPark(int id)
 
       return CreatedAtAction(nameof(GetPark), new { id = park.ParkId }, park);
     }
+
+    // Get individual park with id ap/parks/id
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Park>> GetPark(int id)
+    {
+      var park = await _db.Parks.FindAsync(id);
+
+      if (park == null)
+      {
+          return NotFound();
+      }
+
+      return park;
+  }
+
+   
+    // PUT: api/Parks/5
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Park park)
+    {
+      if (id != park.ParkId)
+      {
+        return BadRequest();
+      }
+
+      _db.Entry(park).State = EntityState.Modified;
+
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!ParkExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+
+      return NoContent();
+    }
+       // DELETE: api/Parks/5
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeletePark(int id)
+    {
+      var park = await _db.Parks.FindAsync(id);
+      if (park == null)
+      {
+        return NotFound();
+      }
+
+      _db.Parks.Remove(park);
+      await _db.SaveChangesAsync();
+
+      return NoContent();
+    }
+   
   }
 }
